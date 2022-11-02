f5aa4be9b5beb773a5c319c4d4c26095ec4682d2
juancruz <juancruzmontanelli@gmail.com>
2022-11-02 11:50:10 -0300

    trabaje en el modelo de productos / dia 1

---
+++ models/product.js
@@ -0,0 +1,91 @@
+const {Sequelize, Model} = require("sequelize");
+const db = require("../config/db")
+
+
+class Product extends Model {}
+
+
+Product.init({
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    name: {
+        type: Sequelize.STRING
+    },
+    brand: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }, 
+    model: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    batteryCapacity: {
+        type: Sequelize.INTEGER,
+        allowNull: false 
+    },
+    screenSize: {
+        type: Sequelize.FLOAT,
+        allowNull: false
+    }, 
+    resolutionX: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    }, 
+    resolutionY: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    },
+    processor: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    },
+    ram: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    },
+    internalStorage: {
+        type: Sequelize.FLOAT,
+        allowNull: false
+    },
+    rearCamera: {
+        type: Sequelize.FLOAT,
+        allowNull: false
+    },
+    frontCamera: {
+        type: Sequelize.FLOAT,
+        allowNull: false
+    },
+    operatingSystem: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    numberOfSims: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    },
+    price: {
+        type: Sequelize.FLOAT,
+        allowNull: false
+    },
+    description: {
+        type: Sequelize.VIRTUAL,
+        get() {
+            return `Este dispositivo cuenta con una capacidad de bateria de ${this.batteryCapacity}, 
+             un tamano de la pantalla de ${this.screenSize},
+             una relsolucion X de ${this.resolutionX}, una relsolucion Y de ${this.resolutionY},
+             un procesador de ${this.processor}, una ram de ${this.ram}, un almacenamineto interno de ${this.internalStorage},
+             una camara Rear de ${this.rearCamera}, una camara frontal de ${this.frontCamera},
+             un sistema operativo de ${this.operatingSystem} y un numero de tarjetas Sims de ${this.numberOfSims}`
+        }
+    }
+}, {sequelize: db, modelName: "product"})
+
+// hooks
+Product.beforeCreate((product) => {
+    return product.name = `${product.brand} ${product.model}`
+})
+
+module.exports = Product
