da769b65cd6cf7e03876f79d5090912db44aaf67
juancruz <juancruzmontanelli@gmail.com>
2022-11-03 16:54:35 -0300

    img

--- controllers/productController.js
+++ controllers/productController.js
@@ -10,41 +10,17 @@ const getOne = (req, res) => {
     .then((product) => res.send(product))
     .catch();
 };
-    const getAll = ((req, res) => {
-        Product.findAll()
-        .then((products) => res.send(products))
-        .catch()
-    })
-    const getOne = ((req, res) => {
-        Product.findByPk(req.params.id)
-        .then((product) => res.send(product))
-        .catch()
-    })
 
-    const create = ((req, res) => {
-        Product.bulkCreate(req.body)
-        .then((products) => res.send(products))
-        .catch()
-    })
-
-    const updateOne = ((req, res) => {
-        const id  = req.params.id 
-        Product.update((req.body) , {where: { id }, returning: true})
-        .then((productUpdate) => res.send(productUpdate))
-        .catch()
-    })
-    const deleteOne = ((req, res) => {
-        const id = req.params.id
-        console.log(req.params.id)
-        Product.destroy({where: { id }})
-        .then(() => res.sendStatus(202))
-        .catch()
-    })
+const create = (req, res) => {
+  Product.bulkCreate(req.body)
+    .then((products) => res.send(products))
+    .catch();
+};
 
 const updateOne = (req, res) => {
   const id = req.params.id;
   Product.update(req.body, { where: { id }, returning: true })
-    .then(([afect, update]) => res.send(update[0]))
+    .then((productUpdate) => res.send(productUpdate))
     .catch();
 };
 const deleteOne = (req, res) => {
@@ -56,9 +32,9 @@ const deleteOne = (req, res) => {
 };
 
 module.exports = {
-    getAll,
-    getOne,
-    create,
-    updateOne,
-    deleteOne 
-}
+  getAll,
+  getOne,
+  create,
+  updateOne,
+  deleteOne,
+};
