a9453dac41abf8a6b5c31a28a360e0919304a815
ltenryuu <cuau_daali@hotmail.com>
2022-11-03 14:59:14 -0600

    Rutas de productos fixed

--- models/product.js
+++ models/product.js
@@ -1,88 +1,103 @@
-const {Sequelize, Model} = require("sequelize");
-const db = require("../config/db")
-
+const { Sequelize, Model } = require("sequelize");
+const db = require("../config/db");
 
 class Product extends Model {}
 
-
-Product.init({
+Product.init(
+  {
     id: {
-        type: Sequelize.INTEGER,
-        autoIncrement: true,
-        primaryKey: true
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    url: {
+      type: Sequelize.STRING,
     },
     name: {
-        type: Sequelize.STRING
+      type: Sequelize.STRING,
     },
     brand: {
-        type: Sequelize.STRING,
-        allowNull: false
-    }, 
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
     model: {
-        type: Sequelize.STRING,
-        allowNull: false
+      type: Sequelize.STRING,
+      allowNull: false,
     },
     batteryCapacity: {
-        type: Sequelize.INTEGER,
-        allowNull: false 
+      type: Sequelize.INTEGER,
+      allowNull: false,
     },
     screenSize: {
-        type: Sequelize.FLOAT,
-        allowNull: false
-    }, 
+      type: Sequelize.FLOAT,
+      allowNull: false,
+    },
     resolutionX: {
-        type: Sequelize.INTEGER,
-        allowNull: false
-    }, 
+      type: Sequelize.INTEGER,
+      allowNull: false,
+    },
     resolutionY: {
-        type: Sequelize.INTEGER,
-        allowNull: false
+      type: Sequelize.INTEGER,
+      allowNull: false,
     },
     processor: {
-        type: Sequelize.INTEGER,
-        allowNull: false
+      type: Sequelize.INTEGER,
+      allowNull: false,
     },
     ram: {
-        type: Sequelize.INTEGER,
-        allowNull: false
+      type: Sequelize.INTEGER,
+      allowNull: false,
     },
     internalStorage: {
-        type: Sequelize.FLOAT,
-        allowNull: false
+      type: Sequelize.FLOAT,
+      allowNull: false,
     },
     rearCamera: {
-        type: Sequelize.FLOAT,
-        allowNull: false
+      type: Sequelize.FLOAT,
+      allowNull: false,
     },
     frontCamera: {
-        type: Sequelize.FLOAT,
-        allowNull: false
+      type: Sequelize.FLOAT,
+      allowNull: false,
     },
     operatingSystem: {
-        type: Sequelize.STRING,
-        allowNull: false
+      type: Sequelize.STRING,
+      allowNull: false,
     },
     numberOfSims: {
-        type: Sequelize.INTEGER,
-        allowNull: false
+      type: Sequelize.INTEGER,
+      allowNull: false,
     },
     price: {
-        type: Sequelize.FLOAT,
-        allowNull: false
+      type: Sequelize.FLOAT,
+      allowNull: false,
     },
     description: {
-        type: Sequelize.VIRTUAL,
-        get() {
-            return `Este dispositivo cuenta con una capacidad de bateria de ${this.batteryCapacity}, un tamano de la pantalla de ${this.screenSize}, una relsolucion X de ${this.resolutionX}, una relsolucion Y de ${this.resolutionY}, un procesador de ${this.processor}, una ram de ${this.ram}, un almacenamineto interno de ${this.internalStorage}, una camara Rear de ${this.rearCamera}, una camara frontal de ${this.frontCamera}, un sistema operativo de ${this.operatingSystem} y un numero de tarjetas Sims de ${this.numberOfSims}`
-        }
-    }
-}, {sequelize: db, modelName: "product"})
+      type: Sequelize.VIRTUAL,
+      get() {
+        return `Este dispositivo cuenta con una capacidad de bateria de ${this.batteryCapacity}, un tamano de la pantalla de ${this.screenSize}, una relsolucion X de ${this.resolutionX}, una relsolucion Y de ${this.resolutionY}, un procesador de ${this.processor}, una ram de ${this.ram}, un almacenamineto interno de ${this.internalStorage}, una camara Rear de ${this.rearCamera}, una camara frontal de ${this.frontCamera}, un sistema operativo de ${this.operatingSystem} y un numero de tarjetas Sims de ${this.numberOfSims}`;
+      },
+    },
+  },
+  { sequelize: db, modelName: "product" }
+);
 
 // hooks
 Product.beforeBulkCreate((products) => {
-    products.map((product) => {
-        return product.name = `${product.brand} ${product.model}`
-    })
-})
+  products.map((product) => {
+    const name = (product.name = `${product.brand} ${product.model}`);
 
-module.exports = Product
+    return name;
+  });
+});
+Product.beforeBulkCreate((products) => {
+  products.map((product) => {
+    const url = (product.url = product.name
+      .replace(/\s+/g, "_")
+      .replace(/\W/g, ""));
+
+    return url;
+  });
+});
+
+module.exports = Product;
